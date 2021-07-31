b1e7ca92874e145535b7b08c3e56cd1d64fc129e
AllaPranathi <88225874+AllaPranathi@users.noreply.github.com>
2021-07-31 16:51:20 +0530

    Update LIfegaurds.java

--- LIfegaurds.java
+++ LIfegaurds.java
@@ -6,8 +6,9 @@ public class LIfegaurds {
            int x, y, z, temp;
            int covered =0;
            int maxCovered =0 ;
-           BufferedReader reader = new BufferedReader(new FileReader("2.in"));
-           PrintWriter writer = new PrintWriter(new BufferedWriter(new PrintWriter("2.out")));
+           int maximum = 0;
+           BufferedReader reader = new BufferedReader(new FileReader("4.in"));
+           PrintWriter writer = new PrintWriter(new BufferedWriter(new PrintWriter("4.out")));
            int N = Integer.parseInt(reader.readLine());
            int[] startArray = new int[N];
            int [] endArray = new int[N];
@@ -16,8 +17,10 @@ public class LIfegaurds {
                    StringTokenizer token = new StringTokenizer(reader.readLine());
                    startArray[x] = Integer.parseInt(token.nextToken());
                    endArray[x] = Integer.parseInt(token.nextToken());
+                   maximum = Math.max(maximum, endArray[x]);
            }
-           int[] interval = new int[1000000000];
+           //int max = Arrays.ma;
+           int[] interval = new int[maximum];
 
            for(x=0; x<N; x++) {
                    for (y = 0; y < N; y++) {
@@ -28,7 +31,7 @@ public class LIfegaurds {
                            }
                    }
                    covered = 0;
-                   for (int i = 0; i < 1000000000; i++) {
+                   for (int i = 0; i < maximum; i++) {
                            if (interval[i] > 0) {
                                    covered++;
                            }
